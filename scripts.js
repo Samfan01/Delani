ec1401154a77f1d632d0f7b6ff36a93ecb0e858d
samfan01 <dsamfan@gmail.com>
2021-01-04 14:31:07 +0300

    added jquery functionality

--- scripts.js
+++ scripts.js
@@ -1,7 +1,21 @@
-var design='#design1'
-
+var test='#design1'
+var user=$('#name').val()
 $(document).ready(function() {
-$('.design').click(function(){
-    $(design).show();
+    $('.design').click(function(){
+    $('.design').hide()
+    $(test).toggle()     
+});
+    $(test).click(function(){
+        $(test).hide()
+        $('.design').toggle()
+    })
+$('.develop').click(function(){
+    $('#development').toggle()
+});
+$('.products').click(function(){
+    $('#product').toggle()
+});
+$('form').submit(function(){
+    alert(user)
 })
 })
